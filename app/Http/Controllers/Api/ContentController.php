1794330c2e74da41e4ba9fcef146ce0491d56b2c
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-05-21 21:12:11 +0900

    index意外のアクションをapiに対応させた

--- app/Http/Controllers/Api/ContentController.php
+++ app/Http/Controllers/Api/ContentController.php
@@ -15,11 +15,6 @@ class ContentController extends Controller
         return $contents;
     }
 
-    public function create()
-    {
-        return view('contents.create');
-    }
-
     public function save(Request $request)
     {
         $new_content = new Content();
@@ -27,20 +22,13 @@ class ContentController extends Controller
         $new_content->detail = $request['detail'];
         $new_content->save();
 
-        return redirect(route('content.index'));
+        return redirect(route('api_content.index'));
     }
 
     public function detail($content_id)
     {
         $content = Content::find($content_id);
-        return view('contents.detail', ['content' => $content]);
-    }
-    
-    public function edit($content_id)
-    {
-        
-        $content = Content::find($content_id);
-        return view('contents.edit', ['content' => $content]);
+        return $content;
     }
 
     public function update($content_id, Request $request)
@@ -49,14 +37,14 @@ class ContentController extends Controller
         $content->title = $request['title'];
         $content->detail = $request['detail'];
         $content->save();
-        return redirect(route('content.index'));
+        return redirect(route('api_content.index'));
     }
 
     public function destroy($content_id)
     {
         $content = Content::find($content_id);
         $content->delete();
-        return redirect(route('content.index'));
+        return redirect(route('api_content.index'));
     }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => ['api']], function() {
-    Route::get('/index', 'Api\ContentController@index')->name('content.index');
-    Route::post('/save', 'Api\ContentController@save')->name('content.save');
-    Route::get('/detail/{content_id}', 'Api\ContentController@detail')->name('content.detail');
-    Route::post('/update/{content_id}', 'Api\ContentController@update')->name('content.update');
-    Route::get('/destroy/{content_id}', 'Api\ContentController@destroy')->name('content.destroy');
+    Route::get('/index', 'Api\ContentController@index')->name('api_content.index');
+    Route::post('/save', 'Api\ContentController@save')->name('api_content.save');
+    Route::get('/detail/{content_id}', 'Api\ContentController@detail')->name('api_content.detail');
+    Route::post('/update/{content_id}', 'Api\ContentController@update')->name('api_content.update');
+    Route::get('/destroy/{content_id}', 'Api\ContentController@destroy')->name('api_content.destroy');
 });
