08b1b9cd120c7aa3865f225cbf029cb3634eab21
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-05-21 19:31:47 +0900

    ビューからの入力を行うルーティング情報を削除した

--- routes/api.php
+++ routes/api.php
@@ -20,10 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => ['api']], function() {
     Route::get('/api/index', 'Api\ContentController@index')->name('content.index');
-    Route::get('/api/create', 'Api\ContentController@create')->name('content.create');
     Route::post('/api/save', 'Api\ContentController@save')->name('content.save');
     Route::get('/api/detail/{content_id}', 'Api\ContentController@detail')->name('content.detail');
-    Route::get('/api/edit/{content_id}', 'Api\ContentController@edit')->name('content.edit');
     Route::post('/api/update/{content_id}', 'Api\ContentController@update')->name('content.update');
     Route::get('/api/destroy/{content_id}', 'Api\ContentController@destroy')->name('content.destroy');
 });
