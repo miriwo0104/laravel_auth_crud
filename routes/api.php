f79180e5fa0c2a75f0cc05ea9565a58e2c05f664
大川峻 <ookawashun@ookawashunnoMacBook-Pro.local>
2020-05-21 19:28:18 +0900

    api用のルーティングを通した

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['api']], function() {
+    Route::get('/api/index', 'Api\ContentController@index')->name('content.index');
+    Route::get('/api/create', 'Api\ContentController@create')->name('content.create');
+    Route::post('/api/save', 'Api\ContentController@save')->name('content.save');
+    Route::get('/api/detail/{content_id}', 'Api\ContentController@detail')->name('content.detail');
+    Route::get('/api/edit/{content_id}', 'Api\ContentController@edit')->name('content.edit');
+    Route::post('/api/update/{content_id}', 'Api\ContentController@update')->name('content.update');
+    Route::get('/api/destroy/{content_id}', 'Api\ContentController@destroy')->name('content.destroy');
+});
